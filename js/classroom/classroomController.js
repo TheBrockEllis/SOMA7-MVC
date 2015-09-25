eec15342cded9ca996a73ad91c49682b8f7b417e
Brock Ellis <brock@sharproot.com>
2015-09-25 16:31:31 -0500

    Classroom Homepage ID Fix

--- js/classroom/classroomController.js
+++ js/classroom/classroomController.js
@@ -3,7 +3,7 @@ define(["app", "js/classroom/classroomView"], function(app, ClassroomView) {
 	//query string is passed to the init function and can then be passed into
 	//the data to get the model or passed directly into the template
 	function init(query) {
-		ClassroomView.render({ title: query.title});
+		ClassroomView.render({ title: query.title, id: query.id});
 	}
 
 	return {
