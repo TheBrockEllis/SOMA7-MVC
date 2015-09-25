ad3fa5e9077858264c3cda7ca8cbefdc03696588
Brock Ellis <brock@sharproot.com>
2015-09-25 15:13:45 -0500

    Class List As Media List

--- js/classes/classesView.js
+++ js/classes/classesView.js
@@ -2,8 +2,6 @@ define(['hbs!js/classes/classes'], function(template) {
 	var $ = Framework7.$;
 
 	function render(params) {
-        $(".classes-header").html("Yo yo yo");
-		//console.log(params);
 		$('.classes-list ul').html(template(params.model));
 	}
 
