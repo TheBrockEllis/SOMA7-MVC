c1cbe17547a545b25e87e023989c02c6d66b1457
Brock Ellis <brock@sharproot.com>
2015-11-18 23:36:26 -0600

    Remove tokens
    Tokes have been revoked and rendered useless

--- js/login/loginController.js
+++ js/login/loginController.js
@@ -17,7 +17,7 @@ define(["app", "js/login/loginView"], function(app, loginView) {
       //hit sycamore api to authenticate user
       //if successful, save access token and redirect user to menu
 	
-	  localStorage.setItem("accessToken", "0c5d5719a496eeb2ed78f5467def9df2");
+	  localStorage.setItem("accessToken", "real access token goes here");
 	  app.f7.accessToken = localStorage.getItem("accessToken");
 	  //console.log(app.mainView);
 	  app.mainView.loadPage("index.html"); 
